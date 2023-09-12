9e98d00b7e5b852c5b7ecb9b20ed11482cb0a828
Vicketori <144226383+Vicketori@users.noreply.github.com>
2023-09-13 00:48:48 +0300

    Add files via upload

---
+++ b4_n30.pas
@@ -0,0 +1,49 @@
+﻿{Дано число X (1 ≤ X ≤ 100). Требуется перевести это число в римскую систему счисления.}
+program b4_n30;
+
+var
+  x: integer;
+
+begin
+  writeln('введите число от 1 до 100 включительно');
+  read(x);
+  write('число ', x, ' в римской системе счисления = ');
+  if (x div 100 = 1)
+    then write('C');
+  if ((x mod 100) div 10 = 9)
+    then write('XC');
+  if ((x mod 100) div 10 = 8)
+    then write('LXXX');
+  if ((x mod 100) div 10 = 7)
+    then write('LXX');
+  if ((x mod 100) div 10 = 6)
+    then write('LX');
+  if ((x mod 100) div 10 = 5)
+    then write('L');
+  if ((x mod 100) div 10 = 4)
+    then write('XL');
+  if ((x mod 100) div 10 = 3)
+    then write('XXX');
+  if ((x mod 100) div 10 = 2)
+    then write('XX');
+  if ((x mod 100) div 10 = 1)
+    then write('X');
+  if (x mod 10 = 9)
+    then write('IX');
+  if (x mod 10 = 8)
+    then write('VIII');
+  if (x mod 10 = 7)
+    then write('VII');
+  if (x mod 10 = 6)
+    then write('VI');
+  if (x mod 10 = 5)
+    then write('V');
+  if (x mod 10 = 4)
+    then write('IV');
+  if (x mod 10 = 3)
+    then write('III');
+  if (x mod 10 = 2)
+    then write('II');
+  if (x mod 10 = 1)
+    then write('I');
+end.
